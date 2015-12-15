6f9df25a54a250f0bd24ef3b4c3fbbcfc2b868f3
david <ogsomgidk>
2015-12-15 14:38:51 -0600

    added "map" to the footer and discarded social media

--- skins/foundation/templates/element.social.php
+++ skins/foundation/templates/element.social.php
@@ -10,10 +10,8 @@
  * License:  GPL-3.0 https://www.gnu.org/licenses/quick-guide-gplv3.html
  *}
 <div class="element-social">
-   <h3>{$LANG.common.follow_us}</h3>
+   <h3>Our Location</h3>
    <ul class="small-block-grid-4 no-bullet nomarg social-icons text-left">
-      {foreach from=$SOCIAL_LINKS item=link}
-      <li><a href="{$link.url}" title="{$link.name}" target="_blank"><i class="fa fa-{$link.icon}"></i></a></li>
-      {/foreach}
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1339.9690953034199!2d-122.57538604170206!3d47.80203389642473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54901f4ac59acd41%3A0xa6eed8fe91bdcf75!2s5534+NE+Minder+Rd%2C+Poulsbo%2C+WA+98370%2C+USA!5e0!3m2!1sen!2sca!4v1450185325508" width="400" height="150"  frameborder="0" style="border:0" allowfullscreen></iframe>
    </ul>
 </div>
